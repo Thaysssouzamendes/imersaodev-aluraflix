069bad25820a498b95dfe2bc6fc3a53b1d0c2ef0
Thays Mendes <123996983+Thaysssouzamendes@users.noreply.github.com>
2023-08-14 19:37:21 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
-var filmeThays = 
+var filmeThays =
   "https://www.showmetech.com.br/wp-content/uploads//2022/12/Avatar-1-1920x1024.jpg";
- 
- var filmeOtavio = 
-   "https://conteudo.imguol.com.br/c/entretenimento/ca/2018/10/02/cena-de-como-treinar-o-seu-dragao-3-1538493470763_v2_3x4.jpg";
-  var filmeGilvania =
-  "https://br.web.img2.acsta.net/medias/nmedia/18/90/89/85/20119071.jpg"; 
 
-     document.write( "<img src=" + filmeThays + ">");
-     document.write( "<img src=" + filmeOtavio + ">");
-     document.write( "<img src=" + filmeGilvania + ">");
+var filmeOtavio =
+  "https://conteudo.imguol.com.br/c/entretenimento/ca/2018/10/02/cena-de-como-treinar-o-seu-dragao-3-1538493470763_v2_3x4.jpg";
+
+var filmeGilvania =
+  "https://br.web.img2.acsta.net/medias/nmedia/18/90/89/85/20119071.jpg";
+
+document.write("<img src='" + filmeThays + "'>");
+document.write("<img src='" + filmeOtavio + "'>");
+document.write("<img src='" + filmeGilvania + "'>");
